60d34650ef019cbac385ced8eba88714057c64b4
Harini-01912 <harinisantosh6669@gmail.com>
2026-09-16 18:40:50 +0530

    redis config fix

--- public/app.js
+++ public/app.js
@@ -1,3 +1,6 @@
+const app = express();
+
+app.set("trust proxy", 1);
 const authView = document.getElementById('authView');
 const dashboardView = document.getElementById('dashboardView');
 const authForm = document.getElementById('authForm');
